e6ef359519ff6a4de75c93818bc6394ef9c09a36
TakuHazard <marwendot@gmail.com>
2020-06-14 15:58:10 -0700

    Added build folder from front end

---
+++ build/precache-manifest.ba354b794b810ec87dca99f4a12fb0be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbb7afdd5a7b4e96f097a7121e6bd648",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f076c370b7df47cfbc50",
+    "url": "/static/css/main.2e126b0c.chunk.css"
+  },
+  {
+    "revision": "6ab7cfdd01ef1e4802d0",
+    "url": "/static/js/2.f8d69277.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f8d69277.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f076c370b7df47cfbc50",
+    "url": "/static/js/main.22868790.chunk.js"
+  },
+  {
+    "revision": "9605c4620dec2e8502b3",
+    "url": "/static/js/runtime-main.e4e63ad3.js"
+  }
+]);
